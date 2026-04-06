78ba42edcfe798ace81b418b0e7a9637cbcc037c
WaseemZia <waseemzia80@gmail.com>
2026-04-06 13:28:48 +0500

    remove error in backend Side

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8be15b97250e799d84df2acc009e515748ddae2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac05a77b00a786080f90118a6fca6b9addbb5fca")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
